6e10222e3180cf2304aed6ddeb463a979d20c083
vianho <silviana.h016@gmail.com>
2024-04-10 20:23:35 +0800

    feat:create envrc component

---
+++ src/components/envrc/envrc.ts
@@ -0,0 +1,22 @@
+import { FileBase, FileBaseOptions, IResolver, Project } from "projen";
+
+export interface EnvrcOptions extends FileBaseOptions {}
+
+// to use with direnv
+// https://direnv.net/
+// brew install direnv
+// echo 'eval "$(direnv hook zsh)"' >> ~/.zshrc
+// direnv allow .
+export class Envrc extends FileBase {
+    constructor(project: Project, filePath: string, options?: EnvrcOptions) {
+        super(project, filePath, options);
+    }
+    protected synthesizeContent(resolver: IResolver): string | undefined {
+        const lines = [
+            ...(this.marker ? [`# ${this.marker}`] : []),
+            "PATH_add .bin",
+        ];
+
+        return `${resolver.resolve(lines).join("\n")}\n`;
+    }
+}
