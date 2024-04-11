1561dd1f57e505d00351f488b11554c767716684
vianho <silviana.h016@gmail.com>
2024-04-11 22:43:04 +0800

    chore:publish to npmjs

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,5 +1,4 @@
 import { GithubCredentials } from 'projen/lib/github';
-import { NpmAccess } from 'projen/lib/javascript';
 import { JSIIProject } from './src/projects';
 
 const project = new JSIIProject({
@@ -12,15 +11,12 @@ const project = new JSIIProject({
   githubOptions: {
     projenCredentials: GithubCredentials.fromApp(),
   },
-  npmAccess: NpmAccess.PUBLIC,
-  npmProvenance: true,
   snykOptions: {
     snykOrgId: 'vianho',
     snykMonitoredProjectId: 'a93b3425-3b4b-4218-b1b1-a67887c0591d',
   },
   peerDeps: ['constructs', 'projen'],
   devDeps: ['constructs', 'projen'],
-  npmRegistryUrl: 'https://npm.pkg.github.com',
   publishToPypi: {
     distName: 'apidays24pj',
     module: 'apidays24pj',
